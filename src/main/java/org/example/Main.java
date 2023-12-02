e3d34c30a4ab499e747adff8b5d2bb014f42ff09
Andrzej <andrzej1250@hotmail.com>
2023-12-02 14:52:34 +0100

    Adding average speed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -30,7 +36,9 @@ public class Main {
         do {
             System.out.println("1. Pobierz położenie ISS");
             System.out.println("2. Pobierz ludzi na ISS");
-            System.out.println("3. Zakończ aplikacje");
+            System.out.println("3. Pokaż prędkość ISS");
+            System.out.println("4. Policz średnią prędkość z pliku");
+            System.out.println("5. Zakończ aplikacje");
 
             choice = scanner.nextInt();
             scanner.nextLine();
@@ -83,6 +91,57 @@ public class Main {
                     break;
 
                 case 3:
+                    final HttpResponse<String> stringHttpResponseFirst = getStringHttpResponse(ISS_API_LOCATION);
+                    final JsonNode jsonNodeFirst = getJsonNode(stringHttpResponseFirst);
+                    final double latFirst = jsonNodeFirst.at("/iss_position/latitude").asDouble();
+                    final double lonFirst = jsonNodeFirst.at("/iss_position/longitude").asDouble();
+
+                    //TODO weź czas z timestampa
+                    final long timeDifferenceInSeconds = 2;
+                    Thread.sleep(Duration.ofSeconds(timeDifferenceInSeconds).toMillis());
+
+                    final HttpResponse<String> stringHttpResponseSecond = getStringHttpResponse(ISS_API_LOCATION);
+                    final JsonNode jsonNodeSecond = getJsonNode(stringHttpResponseSecond);
+                    final double latSecond = jsonNodeSecond.at("/iss_position/latitude").asDouble();
+                    final double lonSecond = jsonNodeSecond.at("/iss_position/longitude").asDouble();
+
+                    final double distance = calculateDistance(latFirst, lonFirst, latSecond, lonSecond);
+
+                    //dorga/przez czas
+                    double speed = distance / timeDifferenceInSeconds;
+                    System.out.println("Iss is going " + speed + "km/s");
+                    writeToCsv("iss_speed.csv", true, "Speed", String.valueOf(speed));
+                    break;
+
+                case 4:
+                    List<String> speedValues = new ArrayList<>();
+                    try (BufferedReader reader = new BufferedReader(new FileReader("iss_speed.csv"))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            final String[] split = line.split(",");
+                            speedValues.add(split[1]);
+                        }
+                    }
+
+                    final Optional<Double> sum = speedValues.stream()
+                            .map(Double::valueOf)
+                            .reduce(Double::sum);
+
+                    double average = 0;
+                    if (sum.isPresent()) {
+                        average = sum.get()/speedValues.size();
+                    }
+
+                    System.out.println("Średnia prędkość z pliku to " + average);
+
+//                  To samo, mniej kodu
+//                    speedValues.stream()
+//                            .mapToDouble(Double::parseDouble)
+//                            .average()
+//                            .ifPresentOrElse(System.out::println, () -> System.out.println("Nie ma średniej"));
+                    break;
+
+                case 5:
                     System.out.println("Zamkykamy appkę");
                     break;
 
@@ -91,7 +150,7 @@ public class Main {
                     break;
             }
 
-        } while (choice != 3);
+        } while (choice != 5);
 
         scanner.close();
     }
@@ -117,8 +176,28 @@ public class Main {
                 line.append(argument).append(",");
             }
             line.append("\n");
+            line.deleteCharAt(line.length() - 2);
             writer.write(line.toString());
         }
 
+    }
+
+
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        // Promień ziemi w kilometrach
+        final double r = 6371;
+
+        // Różnice szerokości i długości geograficznych
+        double deltaLat = Math.toRadians(lat2 - lat1);
+        double deltaLon = Math.toRadians(lon2 - lon1);
+
+        // Obliczenia według wzoru haversine
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Odległość w kilometrach
+        return r * c;
     }
 }
